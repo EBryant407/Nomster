9e89617678e335108dd03978b3e13d03902649d2
Eric <EBryant407@yahoo.com>
2019-08-18 04:02:14 +0000

    added photo and caption

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,7 @@ class PlacesController < ApplicationController
  def show
   @place = Place.find(params[:id])
   @comment = Comment.new
+  @photo = Photo.new
  end
 
  def edit
@@ -62,4 +63,5 @@ class PlacesController < ApplicationController
   params.require(:place).permit(:name, :description, :address)
  end
 
+ 
 end
