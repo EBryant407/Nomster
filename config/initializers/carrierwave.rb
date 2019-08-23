501be00a3cd28eeb35dfbee13c4ce4140c159439
Eric <EBryant407@yahoo.com>
2019-08-23 02:30:26 +0000

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,9 +5,9 @@ CarrierWave.configure do |config|
    config.fog_public = false
    config.fog_credentials = {
       provider:             'AWS',                     # required
-      aws_access_key_id:     ENV["AKIAZQ3YSOQEC3ASKAUW"],    # required
-      aws_secret_access_key: ENV["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"],    # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],    # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],    # required
     }
-    config.fog_directory = ENV["ebryant407-nomster"]            # required
+    config.fog_directory = ENV["AWS_BUCKET"]            # required
     require 'carrierwave/orm/activerecord'
 end
