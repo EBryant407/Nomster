5af1cedf4d33b1e2d0a3003502da176dbeb37016
Eric <EBryant407@yahoo.com>
2019-08-24 23:40:49 +0000

    update

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -7,3 +7,11 @@ class NotificationMailer < ApplicationMailer
   end
 end
 
+class NotificationMailer < ApplicationMailer
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+    mail(to: "ebryant407@yahoo.com",
+      subject: "A comment has been added to your place
+  end
+end
